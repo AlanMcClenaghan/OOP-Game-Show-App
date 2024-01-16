9e2f85cbf92d1e5b11906c8488075b9a8bd7b9c2
Alan McClenaghan <AlanM@Alans-MBP-2.broadband>
2024-01-16 16:58:14 +0000

    Step 4 - Create your phrases

--- js/Game.js
+++ js/Game.js
@@ -7,7 +7,13 @@ class Game {
     constructor() {
         this.missed = 0;
         console.log(this.missed);
-        this.phrases = [];
+        this.phrases = [
+            new Phrase('A piece of cake'),
+            new Phrase('Call it a day'),
+            new Phrase('Once in a blue moon'),
+            new Phrase('The best of both worlds'),
+            new Phrase('The last straw')
+        ];
         console.log(this.phrases);
         this.activePhrase = null;
         console.log(this.activePhrase);
