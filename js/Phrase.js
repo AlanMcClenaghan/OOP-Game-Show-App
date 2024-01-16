6c5829bf43836f08a661ddc032ad44af2b04b4f5
Alan McClenaghan <AlanM@Alans-MBP-2.broadband>
2024-01-16 17:20:59 +0000

    Step 6 - Create a addPhraseToDisplay()

--- js/Phrase.js
+++ js/Phrase.js
@@ -7,4 +7,40 @@ class Phrase {
         this.phrase = phrase.toLowerCase();
         // console.log(this.phrase);
     }
+
+
+    /**
+    * Display phrase on game board
+    */
+    addPhraseToDisplay() {
+        console.log(this.phrase);
+    
+        // Splits phrase into each character
+        const phraseToDisplay = this.phrase.split('');
+        console.log(phraseToDisplay);
+    
+        // Variable to hold code snippet to update HTML
+        let HTML = "";
+    
+        // Each letter is presented by an empty box, one li element for each letter. 
+        // Loop through phrase to construct Phrase to Display
+        phraseToDisplay.forEach(character => {
+            let li = ""
+            if (character === " ") {
+                li = `<li class="space"> </li>`
+            } else {
+                li = `<li class="hide letter ${character}">${character}</li>`
+            }
+            HTML += li
+        });
+        console.log(HTML);
+    
+        // Get UL element in Phrase div
+        const phraseList = document.querySelector('#phrase ul');
+        console.log(phraseList);
+    
+        // Insert Phrase into DOM
+        phraseList.innerHTML = HTML;
+
+    }
 }
