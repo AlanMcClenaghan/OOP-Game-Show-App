2ab52466a956c3f1d7e34d54f3b3fa602468b61b
Alan McClenaghan <AlanM@Alans-MBP-2.broadband>
2024-01-16 18:02:26 +0000

    Step 8 - Start Game button event listener.

--- js/app.js
+++ js/app.js
@@ -2,6 +2,11 @@
  * Project 4 - OOP Game App
  * app.js */
 
-const game = new Game();
-game.startGame();
-console.log(`Active Phrase - phrase: ${game.activePhrase.phrase}`);
+let game;
+
+const button = document.querySelector('#btn__reset');
+
+button.addEventListener('click', () => {
+    game = new Game();
+    game.startGame();
+});
